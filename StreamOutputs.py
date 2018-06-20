e4a8da9c3c3554560096280d8d802f3cc8952ed0
Richard Olij <richardolij@Richards-MacBook-Pro.local>
2018-06-20 12:25:22 +0200

    offline test mode

--- StreamOutputs.py
+++ StreamOutputs.py
@@ -1,18 +1,24 @@
 import matplotlib.pyplot as plt
 import cv2
 
-
+PYTHONW = False
                 
 class StreamOutput:
     
     def __init__(self):
         print('WebsiteOutput')
         
-        cv2.namedWindow('StreamOutput')
-        cv2.startWindowThread()
+        if PYTHONW:
+            cv2.namedWindow('StreamOutput')
+            cv2.startWindowThread()
     
     def update_stream(self, frame):    
         if frame is not None:
             frame_resized = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
-            cv2.imshow('StreamOutput', cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB))
+            
+            if PYTHONW:
+                cv2.imshow('StreamOutput', cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB))
+            else:
+                plt.imshow(frame_resized)
+                plt.show()
 #        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -11,10 +11,12 @@ from StreamOutputs import StreamOutput
 from DroneControllers import DroneController
 from StreamInputs import StreamInput
 
-        
+from Offline import OfflineDroneController, OfflineBebop, OfflineDroneVisionGUI
 
+
+ONLINE = False
         
-if __name__ == "__main__":
+def online():
     bebop = Bebop()
     
 
@@ -46,3 +48,30 @@ if __name__ == "__main__":
 
     bebop.disconnect()
     print('Bye bye')
+    
+def offline():
+    bebop = OfflineBebop()
+
+
+    print('success')
+    # start up the video
+    bebopVision = OfflineDroneVisionGUI(bebop, is_bebop=True, user_args=(bebop, ))
+
+
+
+    droneController = OfflineDroneController(bebop)
+    streamOutput = StreamOutput()
+    
+    keyboardInput = KeyboardInput(droneController)
+    thread_KeyboardInput = Thread(target = keyboardInput.wait_for_input, args = ( ))
+    thread_KeyboardInput.start()
+    
+    stream_input = StreamInput(bebopVision, streamOutput, droneController)
+    while True:
+        stream_input.processing_stream(None)
+    
+if __name__ == "__main__":
+    if ONLINE:
+        online()
+    else:
+        offline()
